02497567eecd1e310587d5fc3a08613f7d30c94e
huriuraz <huri_uraz@hotmail.com>
2014-10-27 18:58:46 +0200

    bolum fonksiyonunu ekledim

--- main.c
+++ main.c
@@ -1,5 +1,38 @@
-#include "stdio.h"
+#include <stdio.h>
 
-int main() {
-	return 0;
+
+ int main()
+{
+    int sayi1,sayi2;
+    printf("1.sayiyi giriniz\n");
+    scanf("%d",&sayi1 );
+    printf("2.sayiyi giriniz\n");
+    
+    
+    
+    scanf("%d",&sayi2);
+    
+    bolum(sayi1,sayi2);
+    
+    
+    return 0;
 }
+
+void bolme(int x, int y){
+   if(y==0)
+   {
+       printf("sonuc tanimsizdir");
+       
+   }
+   else
+   {
+   float sonuc=x/y;
+   
+   printf("sonuc %f\n",sonuc);
+   }
+   
+   
+   
+   
+   
+
